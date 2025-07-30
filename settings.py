a1a78cc1455897498d9303f8d026cce68b65cf4c
Daiki Nagata <skram.uiglu@gmail.com>
2025-07-30 21:28:08 +0900

    room

--- settings.py
+++ settings.py
@@ -35,7 +35,6 @@ ROOMS = [
     dict(
         name='econ101',
         display_name='Econ 101 class',
-        participant_label_file='_rooms/econ101.txt',
     ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
